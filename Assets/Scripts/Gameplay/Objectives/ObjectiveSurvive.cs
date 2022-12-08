3f2c063a974b5a22e955f7c7412952a741cb646d
Curtis-Jensen <me@curtisjensen.com>
2022-12-08 16:16:34 -0700

    Changed the name of the objective without destroying everything!

---
+++ Assets/Scripts/Gameplay/Objectives/ObjectiveSurvive.cs
@@ -0,0 +1,79 @@
+﻿using Unity.FPS.Game;
+using UnityEngine;
+
+namespace Unity.FPS.Gameplay
+{
+    public class ObjectiveSurvive : Objective
+    {
+        [Tooltip("Chose whether you need to kill every enemies or only a minimum amount")]
+        public bool MustKillAllEnemies = true;
+
+        [Tooltip("If MustKillAllEnemies is false, this is the amount of enemy kills required")]
+        public int KillsToCompleteObjective = 5;
+
+        [Tooltip("Start sending notification about remaining enemies when this amount of enemies is left")]
+        public int NotificationEnemiesRemainingThreshold = 3;
+
+        int killTotal;
+
+        protected override void Start()
+        {
+            base.Start();
+
+            EventManager.AddListener<EnemyKillEvent>(OnEnemyKilled);
+
+            // set a title and description specific for this type of objective, if it doesn't have one
+            if (string.IsNullOrEmpty(Title))
+                Title = "Eliminate " + (MustKillAllEnemies ? "all the" : KillsToCompleteObjective.ToString()) +
+                        " enemies";
+
+            if (string.IsNullOrEmpty(Description))
+                Description = GetUpdatedCounterAmount();
+        }
+
+        void OnEnemyKilled(EnemyKillEvent evt)
+        {
+            if (IsCompleted)
+                return;
+
+            killTotal++;
+
+            if (MustKillAllEnemies)
+                KillsToCompleteObjective = evt.RemainingEnemyCount + killTotal;
+
+            int targetRemaining = MustKillAllEnemies ? evt.RemainingEnemyCount : KillsToCompleteObjective - killTotal;
+
+            // update the objective text according to how many enemies remain to kill
+            if (targetRemaining == 0)
+            {
+                CompleteObjective(string.Empty, GetUpdatedCounterAmount(), "Objective complete : " + Title);
+            }
+            else if (targetRemaining == 1)
+            {
+                string notificationText = NotificationEnemiesRemainingThreshold >= targetRemaining
+                    ? "One enemy left"
+                    : string.Empty;
+                UpdateObjective(string.Empty, GetUpdatedCounterAmount(), notificationText);
+            }
+            else
+            {
+                // create a notification text if needed, if it stays empty, the notification will not be created
+                string notificationText = NotificationEnemiesRemainingThreshold >= targetRemaining
+                    ? targetRemaining + " enemies to kill left"
+                    : string.Empty;
+
+                UpdateObjective(string.Empty, GetUpdatedCounterAmount(), notificationText);
+            }
+        }
+
+        string GetUpdatedCounterAmount()
+        {
+            return $"{killTotal} / {KillsToCompleteObjective}";
+        }
+
+        void OnDestroy()
+        {
+            EventManager.RemoveListener<EnemyKillEvent>(OnEnemyKilled);
+        }
+    }
+}
