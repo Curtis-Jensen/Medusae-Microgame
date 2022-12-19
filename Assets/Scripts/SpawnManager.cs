cbdbefe2a4a7a519fdf0bbd9eec35fde98660c46
Curtis-Jensen <me@curtisjensen.com>
2022-12-19 16:36:09 -0700

    Cleaned up Spawn Manager a bit

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,41 +4,32 @@ using UnityEngine.UI;
 public class SpawnManager : MonoBehaviour
 {
     #region Variables
-    [Tooltip("We need to cap total enemies for performance.")]
-    public int enemyCap;
     public int waveNumber;
-    [Tooltip("The amount of seconds it takes for the next wave to start automatically")]
-    public float waveTimeLimit;
-    [Range(0, 1)]
+    [Range(0, 1)] [Tooltip("The percent chance the enemy will be a medusa or a hoverbot")]
     public float medusaChance;
     public GameObject medusaPrefab;
     public GameObject hoverBotPrefab;
-    public Transform sunTransform;
     public Text waveHud;
-    [Tooltip("Will the map be regenerated with each new wave?")]
+    [Tooltip("Whether or not the map be regenerated with each new wave")]
     public bool generatingChaotically;
     public ProceduralGeneration mapGenerator;
 
-    Transform spawnerList;
-    bool nextWave = false;
-    float waveTimer;
     string playerName;
     #endregion
 
     /* Keeps track of all the different groups that are being pooled
-        * 
-        * If the wave number is set to 0 that actually means that wave saving is on and it will automatically
-        * chose a good wave
-        * 
-        * The reason the wave number is subtracted here is because EndWave() advances the wave,
-        * even if it's the very first wave.  I used to have to have the starting wave always be 0 to
-        * circummvent, but this is automatic.
-        * 
-        * And pauses the game in the beginning so the player knows how to play or that they've died
-        */
+     * 
+     * If the wave number is set to 0 that actually means that wave saving is on and it will automatically
+     * chose a good wave
+     * 
+     * The reason the wave number is subtracted here is because EndWave() advances the wave,
+     * even if it's the very first wave.  I used to have to have the starting wave always be 0 to
+     * circummvent, but this is automatic.
+     * 
+     * And pauses the game in the beginning so the player knows how to play or that they've died
+     */
     void Awake()
     {
-        spawnerList = GameObject.Find("Enemy Spawn Points").transform;
         playerName = PlayerPrefs.GetString("playerName");
         if (waveNumber == 0)
             waveNumber = PlayerPrefs.GetInt(playerName + "waveNumber") / 2;
@@ -49,8 +40,8 @@ public class SpawnManager : MonoBehaviour
     }
 
     /* Advances the wave, calls spawning, and tells the appropriate scripts about the
-        * increased threat
-        */
+     * increased threat
+     */
     public void EndWave()
     {
         waveNumber++;
@@ -60,25 +51,28 @@ public class SpawnManager : MonoBehaviour
 
         waveHud.text = waveNumber.ToString();
 
-        //if(generatingChaotically) mapGenerator.CreateMap();
+        if(generatingChaotically) mapGenerator.CreateMap();
     }
 
     /* Spawns as many enemies as there are waves, and makes sure to leave active enemies alone.
-        */
+     * 
+     * Randomly determines what the spawn point will be based off the list of spawn points, 
+     * which are determined by what the children are
+     */
     void SpawnEnemies()
     {
         GameObject chosenEnemy;
 
         for (int i = 0; i < waveNumber; i++)
         {
-            if (Random.value > medusaChance)
+            if (Random.value < medusaChance)
                 chosenEnemy = medusaPrefab;
             else
                 chosenEnemy = hoverBotPrefab;
 
-            var nextSpawn = spawnerList.GetChild(Random.Range(0, spawnerList.childCount)).gameObject;
+            var nextSpawn = transform.GetChild(Random.Range(0, transform.childCount));
 
-            Instantiate(chosenEnemy, nextSpawn.transform.position, Quaternion.identity, gameObject.transform);
+            Instantiate(chosenEnemy, nextSpawn.position, Quaternion.identity, gameObject.transform);
         }
     }
 }
