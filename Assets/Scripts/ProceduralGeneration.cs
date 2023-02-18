f283733fe1308a12670e07fd50dec3ffa207eb48
Curtis-Jensen <me@curtisjensen.com>
2023-02-18 14:54:26 -0700

    Put in the random chance for a thinck maze

--- Assets/Scripts/ProceduralGeneration.cs
+++ Assets/Scripts/ProceduralGeneration.cs
@@ -3,12 +3,16 @@ using UnityEngine.AI;
 
 public class ProceduralGeneration : MonoBehaviour
 {
-    [Header("Inputs")]
+    [Header("General")]
     [Tooltip("The floor needs to be kept in mind so the nav meshes can be baked.")]
     public NavMeshSurface floor;
-
-    [Tooltip("The wall that will be sprinkled around")]
-    public GameObject wallPrefab;
+    [Tooltip("The wall, tree, or other object that will be sprinkled around")]
+    public GameObject objectPrefab;
+    [Tooltip("If true: objects spawn on spawn points. if false: objects spawn within a region")]
+    public bool spawnPointBased = false;
+    [Range(0, 1)]
+    [Tooltip("The chance that the objects will spawn as much as possible, making a maze")]
+    public float packedMazeChance;
 
     [Header("Dimensions")]
     [Tooltip("How far, multiplied by 10, the walls will go.")]
@@ -23,7 +27,7 @@ public class ProceduralGeneration : MonoBehaviour
     public float tiltAngle = 10;
     [Range(0, 1)]
     [Tooltip("What percentage of the map will be filled with walls")]
-    public float wallChance;
+    public float objectPrefabChance;
 
     [Header("Forest")]
     [Tooltip("Whether forestey behaviors such as stretching will occur")]
@@ -50,25 +54,44 @@ public class ProceduralGeneration : MonoBehaviour
      */
     public void CreateMap()
     {
-        var children = gameObject.GetComponentsInChildren<Transform>();
+        float currentObjectPrefabChance;
+        if (Random.value < packedMazeChance) currentObjectPrefabChance = 1;
+        else                                 currentObjectPrefabChance = objectPrefabChance;
 
+        var children = gameObject.GetComponentsInChildren<Transform>();
         for (int i = 1; i < children.Length; i++) // 10
             DestroyImmediate(children[i].gameObject);
 
+        PlaceObjectsInRegion(currentObjectPrefabChance);
+
+        floor.BuildNavMesh(); // 50
+    }
+
+    /// <summary>
+    /// Places objects based off the region that is specified
+    /// </summary>
+    void PlaceObjectsInRegion(float currentObjectPrefabChance)
+    {
         for (int x = widthMin; x < widthMax; x++) // 20
             for (int z = lengthMin; z < lengthMax; z++)
             {
-                if (Random.value > wallChance) continue; // 30
+                if (Random.value > currentObjectPrefabChance) continue; // 30
 
                 var position = new Vector3(x * 10, 2, z * 10);
-                var rotation = 
+                var rotation =
                     new Vector3(Random.Range(-tiltAngle, tiltAngle), Random.Range(0, 360), Random.Range(-tiltAngle, tiltAngle)); // 40
-                var newPrefab = Instantiate(wallPrefab, position, Quaternion.Euler(rotation), gameObject.transform);
+                var newPrefab = Instantiate(objectPrefab, position, Quaternion.Euler(rotation), gameObject.transform);
 
-                if(forest)
+                if (forest)
                     newPrefab.transform.localScale = new Vector3(Random.Range(1, stretchAmounts), Random.Range(1, stretchAmounts), Random.Range(1, stretchAmounts));
             }
+    }
 
-        floor.BuildNavMesh(); // 50
+    /// <summary>
+    /// Places the objects based off the spawn points that are laid out
+    /// </summary>
+    void PlaceObjectsByPoints(float currentObjectPrefabChance)
+    {
+
     }
 }
