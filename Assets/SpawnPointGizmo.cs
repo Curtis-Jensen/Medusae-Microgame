b282127121fede074187d888ac86cf97833e9c7d
Curtis-Jensen <me@curtisjensen.com>
2023-04-11 12:20:42 -0600

    Placed a ton of spawn points

--- Assets/SpawnPointGizmo.cs
+++ Assets/SpawnPointGizmo.cs
@@ -5,6 +5,12 @@ public class SpawnPointGizmo : MonoBehaviour
     public Color gizmoColor = Color.green;
     public float gizmoSize = 0.5f;
 
+    private void OnMouseDown()
+    {
+        // Select the game object in the Scene view
+        UnityEditor.Selection.activeGameObject = gameObject;
+    }
+
     void OnDrawGizmos()
     {
         Gizmos.color = gizmoColor;
