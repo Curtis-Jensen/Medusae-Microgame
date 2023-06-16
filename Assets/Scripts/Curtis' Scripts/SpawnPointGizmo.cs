133e5aacf171e781c1d0f43ada60f12da8ee05a2
Curtis-Jensen <me@curtisjensen.com>
2023-06-16 16:24:57 -0600

    Fixed a unity building error

--- Assets/Scripts/Curtis' Scripts/SpawnPointGizmo.cs
+++ Assets/Scripts/Curtis' Scripts/SpawnPointGizmo.cs
@@ -5,6 +5,7 @@ public class SpawnPointGizmo : MonoBehaviour
     public Color gizmoColor = Color.green;
     public float gizmoSize = 0.5f;
 
+#if UNITY_EDITOR
     private void OnMouseDown()
     {
         // Select the game object in the Scene view
@@ -16,4 +17,5 @@ public class SpawnPointGizmo : MonoBehaviour
         Gizmos.color = gizmoColor;
         Gizmos.DrawWireCube(transform.position, Vector3.one * gizmoSize);
     }
+#endif
 }
