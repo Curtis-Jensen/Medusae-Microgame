3f3d4ec6982075d1eb0b11542c875afe2318ae0f
Curtis-Jensen <me@curtisjensen.com>
2023-08-22 12:30:46 -0600

    GPT wrote stuff for me!

--- Assets/Scripts/Curtis' Scripts/GenerateByRegion.cs
+++ Assets/Scripts/Curtis' Scripts/GenerateByRegion.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 
 public class GenerateByRegion : ProceduralGeneration
 {
-    /*  🧨 For each spawn point, delete any existing walls and spawn a new object if the random value is below the prefab chance
+    public Color gizmoColor = Color.blue;
+    public Vector3 regionSize = new Vector3(10f, 1f, 10f);
 
-            💥 Delete existing walls from spawn points immediately so it can be used in the inspector
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireCube(transform.position, regionSize);
+    }
 
-            🧱 Spawn new prefab with the spawnpoint's position, the generated rotation, and with the spawn point as a parent
-
-            🎉 Instantiate the prefab with the spawn point as its parent
-
-            🎨 Stretch the prefab if the stretchy flag is set
-         */
     /// <summary>
     /// Places the objects based off the spawn points that are laid out
     /// </summary>
     /// <param name="spawnChance">The chance of spawning a new object on each spawn point</param>
     protected override void SpawnObjects(float spawnChance)
     {
-        Vector3 regionSize = new Vector3(10f, 0f, 10f); // Change this to your desired region size
         Vector3 spawnPointOffset = new Vector3(0.5f, 0f, 0.5f); // Offset to ensure objects are centered
 
         for (float x = 0; x < regionSize.x; x++)
@@ -46,7 +44,7 @@ public class GenerateByRegion : ProceduralGeneration
                                                 Random.Range(0, 360),
                                                 Random.Range(-tiltAngle, tiltAngle));
 
-                var newObject = Instantiate(chosenObject, spawnPosition, rotation); // No parent here
+                var newObject = Instantiate(chosenObject, spawnPosition, rotation, transform);
 
                 if (stretchy)
                     newObject.transform.localScale = new Vector3(Random.Range(1, stretchAmounts),
