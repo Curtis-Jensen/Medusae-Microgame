5466c21433932b30cb790fe6c71ca0015a71f6ec
Curtis-Jensen <me@curtisjensen.com>
2023-08-23 09:33:30 -0600

    Added wind lines!

--- Assets/SpeedLines.cs
+++ Assets/SpeedLines.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class SpeedLines : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speedThreshold = 30f; // Adjust this value to your desired threshold
+
+    ParticleSystem speedLinesParticleSystem;
+    Vector3 previousPosition;
+    bool creatingParticles = false;
+
     void Start()
     {
-        
+        // Initialize the previous position to the player's starting position
+        previousPosition = transform.position;
+
+        speedLinesParticleSystem = gameObject.GetComponent<ParticleSystem>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Calculate the player's falling speed
+        float playerSpeed = CalculateFallingSpeed();
+
+        //Debug.Log(speedLinesParticleSystem.isPlaying);
+
+        if (playerSpeed < speedThreshold)
+        {
+            // Turn on the particle system
+            if (!creatingParticles)
+            {
+                speedLinesParticleSystem.Play();
+                creatingParticles = true;
+            }
+        }
+        else
+        {
+            // Turn off the particle system
+            if (creatingParticles)
+            {
+                speedLinesParticleSystem.Stop();
+                creatingParticles = false;
+            }
+        }
+
+        // Update the previous position for the next frame
+        previousPosition = transform.position;
+    }
+
+    float CalculateFallingSpeed()
+    {
+        // Calculate the distance traveled in the vertical axis
+        float verticalDistance = transform.position.y - previousPosition.y;
+
+        // Calculate falling speed using time.deltaTime to normalize the value
+        float fallingSpeed = verticalDistance / Time.deltaTime;
+
+        Debug.Log(fallingSpeed);
+
+        return fallingSpeed;
     }
 }
