3b5160e5cddfe7662201775839e7238da768da48
Curtis-Jensen <me@curtisjensen.com>
2023-08-23 10:13:33 -0600

    tweaked particles more

--- Assets/SpeedLines.cs
+++ Assets/SpeedLines.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class SpeedLines : MonoBehaviour
 {
     public float speedThreshold = 30f; // Adjust this value to your desired threshold
+    public float minFOV = 60f; // Adjust the minimum FOV value
+    public float maxFOV = 90f; // Adjust the maximum FOV value
+    public float smoothingFactor = 0.1f; // Adjust this factor for smoothing
+    public Camera cam;
 
     ParticleSystem speedLinesParticleSystem;
+    float smoothedSpeed = 0f;
     Vector3 previousPosition;
-    bool creatingParticles = false;
+    bool fallingFast = false;
 
     void Start()
     {
         // Initialize the previous position to the player's starting position
         previousPosition = transform.position;
-
+    
         speedLinesParticleSystem = gameObject.GetComponent<ParticleSystem>();
     }
 
@@ -23,26 +28,33 @@ public class SpeedLines : MonoBehaviour
         // Calculate the player's falling speed
         float playerSpeed = CalculateFallingSpeed();
 
-        //Debug.Log(speedLinesParticleSystem.isPlaying);
-
         if (playerSpeed < speedThreshold)
         {
             // Turn on the particle system
-            if (!creatingParticles)
+            if (!fallingFast)
             {
                 speedLinesParticleSystem.Play();
-                creatingParticles = true;
+                fallingFast = true;
             }
         }
         else
         {
             // Turn off the particle system
-            if (creatingParticles)
+            if (fallingFast)
             {
                 speedLinesParticleSystem.Stop();
-                creatingParticles = false;
+                fallingFast = false;
             }
         }
+
+        // Smooth the falling speed value
+        smoothedSpeed = Mathf.Lerp(smoothedSpeed, playerSpeed, smoothingFactor);
+
+        // Calculate FOV based on smoothed falling speed
+        float targetFOV = Mathf.Lerp(minFOV, maxFOV, smoothedSpeed / speedThreshold);
+
+        // Apply the target FOV to the camera's field of view
+        cam.fieldOfView = targetFOV;
 
         // Update the previous position for the next frame
         previousPosition = transform.position;
@@ -55,8 +67,6 @@ public class SpeedLines : MonoBehaviour
 
         // Calculate falling speed using time.deltaTime to normalize the value
         float fallingSpeed = verticalDistance / Time.deltaTime;
-
-        Debug.Log(fallingSpeed);
 
         return fallingSpeed;
     }
