bc92f4d8f98882d31866d0971f05c8bea056157f
Curtis-Jensen <me@curtisjensen.com>
2023-08-23 21:50:31 -0600

    Made the tree

--- Assets/EndlessGeneration.cs
+++ Assets/EndlessGeneration.cs
@@ -28,7 +28,7 @@ public class EndlessGeneration : MonoBehaviour
     {
         var newPosition =
             new Vector3(lastSpawned.position.x, lastSpawned.position.y - 100, lastSpawned.position.z); // Calculate where the new floor will go
-                GameObject newFloor = Instantiate(floor, newPosition, Quaternion.identity, transform); // Spawn the new floor
+        GameObject newFloor = Instantiate(floor, newPosition, Quaternion.identity, transform); // Spawn the new floor
 
         lastSpawned = newFloor.transform; // Keep track of where the last floor was to know where the next one goes
 
