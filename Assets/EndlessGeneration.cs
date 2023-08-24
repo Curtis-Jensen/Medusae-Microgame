7f10182dd26e82120132a3f156791b97ee94b1b1
Curtis-Jensen <me@curtisjensen.com>
2023-08-23 19:38:22 -0600

    made a new floor

--- Assets/EndlessGeneration.cs
+++ Assets/EndlessGeneration.cs
@@ -7,16 +7,41 @@ public class EndlessGeneration : MonoBehaviour
     public Transform player;
     public Transform lastSpawned;
     public GameObject floor;
+    public GameObject treeFloor;
     public float loadingDistance = 20;
+    public int floorsUntilTree = 20;
+
+    int floorsSpawned;
 
     void Update()
     {
-        if (player.position.y > (lastSpawned.position.y + loadingDistance)) return;
+        if (player.position.y > (lastSpawned.position.y + loadingDistance)) return; // If the player hasn't fallen far enough, don't make a new floor
 
+        if (floorsSpawned == floorsUntilTree)
+            SpawnTree();
+        else
+            SpawnNewFloor();
+    }
+
+    void SpawnNewFloor()
+    {
         var newPosition =
-            new Vector3(lastSpawned.position.x, lastSpawned.position.y - 100, lastSpawned.position.z);
-        GameObject newFloor = Instantiate(floor, newPosition, Quaternion.identity, transform);
+            new Vector3(lastSpawned.position.x, lastSpawned.position.y - 100, lastSpawned.position.z); // Calculate where the new floor will go
+                GameObject newFloor = Instantiate(floor, newPosition, Quaternion.identity, transform); // Spawn the new floor
 
-        lastSpawned = newFloor.transform;
+        lastSpawned = newFloor.transform; // Keep track of where the last floor was to know where the next one goes
+
+        floorsSpawned++;
+    }
+
+    void SpawnTree()
+    {
+        var newPosition =
+            new Vector3(lastSpawned.position.x, lastSpawned.position.y - 100, lastSpawned.position.z); // Calculate where the new floor will go
+        GameObject newFloor = Instantiate(treeFloor, newPosition, Quaternion.identity, transform); // Spawn the new floor
+
+        lastSpawned = newFloor.transform; // Keep track of where the last floor was to know where the next one goes
+
+        floorsSpawned++;
     }
 }
