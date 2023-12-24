b7c72cda12bc085b12f9478ecd049e362f2e0705
Curtis-Jensen <me@curtisjensen.com>
2023-12-23 20:01:56 -0700

    Changed some of the wording for highscores and cleaned up the midnight wood song

--- Assets/HighScoreDisplay.cs
+++ Assets/HighScoreDisplay.cs
@@ -8,23 +8,29 @@ public class HighScoreDisplay : MonoBehaviour
 {
     TextMeshProUGUI highScoreDisplay;
     [TextArea(1, 10)]
-    public string highScoreMessage;    
+    public string[] highScoreMessages;    
     [TextArea(1, 10)]
-    public string regularMessage;
+    public string[] regularMessages;
 
     void Start()
     {
+        // Instantiates objects and gets high scores
         highScoreDisplay = GetComponent<TextMeshProUGUI>();
 
         var recentScore = PlayerPrefs.GetInt("recentScore");
         var highScore = PlayerPrefs.GetInt("highScore");
 
+        // Decides if it's a high score or a regular score and displays the message
         if (recentScore > highScore)
         {
             PlayerPrefs.SetInt("highScore", recentScore);
-            highScoreDisplay.text = string.Format(highScoreMessage, recentScore); ;
+            int random = Random.Range(0, highScoreMessages.Length);
+            highScoreDisplay.text = string.Format(highScoreMessages[random], recentScore); ;
         }
         else
-            highScoreDisplay.text = string.Format(regularMessage, recentScore, highScore); ;
+        {
+            int random = Random.Range(0, regularMessages.Length);
+            highScoreDisplay.text = string.Format(regularMessages[random], recentScore, highScore);
+        }
     }
 }
